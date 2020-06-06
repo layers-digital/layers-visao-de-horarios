fae8706bc741a2918a314f097f559fd8d777119a
lucas-grippa <lucas-grippa@hotmail.com>
2020-06-06 20:28:33 -0300

    Init firebase analytics and performance

--- web/src/main.js
+++ web/src/main.js
@@ -10,6 +10,26 @@ import Transitions from './styles/Transitions.css'
 import Text from './styles/Text.css'
 import Utils from './styles/Utils.css'
 import Environment from './environment'
+import * as firebase from "firebase/app"
+import "firebase/analytics"
+import "firebase/performance"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClHapIUfABfG7HlGLX7MldyTJMHqlO36w",
+  authDomain: "layers-visao-de-horarios.firebaseapp.com",
+  databaseURL: "https://layers-visao-de-horarios.firebaseio.com",
+  projectId: "layers-visao-de-horarios",
+  storageBucket: "layers-visao-de-horarios.appspot.com",
+  messagingSenderId: "357333412353",
+  appId: "1:357333412353:web:1e37658fa7c596e5a36e00",
+  measurementId: "G-230LP96Q3H"
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+firebase.performance()
+
+Vue.prototype.$analytics = firebase.analytics()
 
 Vue.config.productionTip = false
 
