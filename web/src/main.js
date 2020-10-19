d03be7cfcb29e5e9a3ee0e2561cce02253e56434
André Sakuma <andre.sakuma@tenda.digital>
2020-10-19 17:43:19 -0300

    transform store into a function createStore

--- web/src/main.js
+++ web/src/main.js
@@ -1,18 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import Axios from 'axios'
-import store from './store'
-import General from './styles/General.css'
-import Grid from './styles/Grid.css'
-import Colors from './styles/Colors.css'
-import Transitions from './styles/Transitions.css'
-import Text from './styles/Text.css'
-import Utils from './styles/Utils.css'
-import Environment from './environment'
-import * as firebase from "firebase/app"
-import "firebase/analytics"
-import "firebase/performance"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Axios from "axios";
+import createStore from "./store";
+import getQueryVariable from "@/helpers/getQueryVariable";
+import General from "./styles/General.css";
+import Grid from "./styles/Grid.css";
+import Colors from "./styles/Colors.css";
+import Transitions from "./styles/Transitions.css";
+import Text from "./styles/Text.css";
+import Utils from "./styles/Utils.css";
+import Environment from "./environment";
+import * as firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/performance";
 
 const firebaseConfig = {
   apiKey: "AIzaSyClHapIUfABfG7HlGLX7MldyTJMHqlO36w",
@@ -22,21 +23,36 @@ const firebaseConfig = {
   storageBucket: "layers-visao-de-horarios.appspot.com",
   messagingSenderId: "357333412353",
   appId: "1:357333412353:web:1e37658fa7c596e5a36e00",
-  measurementId: "G-230LP96Q3H"
-}
+  measurementId: "G-230LP96Q3H",
+};
 
-firebase.initializeApp(firebaseConfig)
-firebase.analytics()
-firebase.performance()
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.performance();
 
-Vue.prototype.$analytics = firebase.analytics()
+Vue.prototype.$analytics = firebase.analytics();
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Axios.defaults.baseURL = Environment.API_URL
+Axios.defaults.baseURL = Environment.API_URL;
 
 async function init() {
-  await store.dispatch('layers/init')
+  let communityId, userId;
+  try {
+    await LayersPortal.connectedPromise;
+    communityId = LayersPortal.communityId;
+    userId = LayersPortal.userId;
+  } catch (err) {
+    communityId = getQueryVariable("community");
+    userId = getQueryVariable("token");
+  }
+
+  const store = createStore({
+    communityId,
+    userId,
+  });
+
+  await store.dispatch("layers/init");
 
   new Vue({
     router,
@@ -48,8 +64,10 @@ async function init() {
     Text,
     Utils,
     Axios,
-    render: function (h) { return h(App) }
-  }).$mount('#app')
+    render: function (h) {
+      return h(App);
+    },
+  }).$mount("#app");
 }
 
-init()
+init();
--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,25 +1,37 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import getQueryVariable from '@/helpers/getQueryVariable'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import timetables from './timetables'
-import layers from './layers'
+import timetables from "./timetables";
+import layers from "./layers";
 
-import persistedState from 'vuex-persistedstate';
+import persistedState from "vuex-persistedstate";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const community = getQueryVariable('community')
+export default function createStore({ communityId, userId }) {
+  let options;
+  try {
+    options = {
+      strict: true,
+      plugins: [
+        persistedState({
+          key: `@${communityId}:${userId}-layers-visao-de-horarios`,
+        }),
+      ],
+      modules: {
+        layers,
+        timetables,
+      },
+    };
+  } catch (err) {
+    options = {
+      strict: true,
+      modules: {
+        layers,
+        timetables,
+      },
+    };
+  }
 
-export default new Vuex.Store({
-  strict: true,
-  plugins: [
-    persistedState({
-      key: community
-    })
-  ],
-  modules: {
-    layers,
-    timetables
-  },
-})
+  return new Vuex.Store(options);
+}
