397772ed96d249a0cbd09bd315f243801ef70824
André Sakuma <andre.sakuma@tenda.digital>
2020-10-20 14:14:13 -0300

    fix store and layers.js

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -9,29 +9,30 @@ import persistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 export default function createStore({ communityId, userId }) {
-  let options;
-  try {
-    options = {
-      strict: true,
-      plugins: [
-        persistedState({
-          key: `@${communityId}:${userId}-layers-visao-de-horarios`,
-        }),
-      ],
-      modules: {
-        layers,
-        timetables,
-      },
-    };
-  } catch (err) {
-    options = {
-      strict: true,
-      modules: {
-        layers,
-        timetables,
-      },
-    };
-  }
+  const options = {
+    strict: true,
+    plugins: [],
+    modules: {
+      layers,
+      timetables,
+    },
+  };
 
+  // Enable persisted state if user's LocalStore is enabled
+  if (isLocalStorageEnabled()) {
+    options.plugins.push(
+      persistedState({
+        key: `@${communityId}:${userId}-layers-visao-de-horarios`,
+      })
+    );
+  }
   return new Vuex.Store(options);
+}
+
+function isLocalStorageEnabled() {
+  try {
+    return window.localStorage instanceof Storage;
+  } catch (error) {
+    return false;
+  }
 }
--- web/src/store/layers.js
+++ web/src/store/layers.js
@@ -5,85 +5,23 @@ const state = {
   userId: null,
   communityId: null,
   accountId: null,
-  bridgeConnected: false,
-};
-
-const mutations = {
-  setSession(context, session) {
-    context.session = session;
-  },
-
-  setUserId(context, userId) {
-    context.userId = userId;
-  },
-
-  setCommunityId(context, communityId) {
-    context.communityId = communityId;
-  },
-
-  setAccountId(context, accountId) {
-    context.accountId = accountId;
-  },
-
-  setBridgeConnected(context, bridgeConnected) {
-    context.bridgeConnected = bridgeConnected;
-  },
 };
 
 const actions = {
-  async init(context) {
-    context.commit("setBridgeConnected", false);
-
-    try {
-      // Check if has token in query params and ignore Layers SDK connection
-      const token = getQueryVariable("token");
-      if (token) {
-        throw new Error("Deprecated authenticate method");
-      }
-
-      if (!LayersPortal) {
-        throw new Error("LayersPortal is not defined");
-      }
-
-      await LayersPortal.readyPromise;
-      if (!LayersPortal.platform || !LayersPortal.connected) {
-        throw new Error("Layers Portal not connected");
-      }
-
-      (await LayersPortal) && LayersPortal.connectedPromise;
-      const session = LayersPortal.session;
-      const communityId = LayersPortal.communityId;
-      const userId = LayersPortal.userId;
-      const accountId = LayersPortal.accountId;
-      const connected = LayersPortal.connected;
-
-      context.dispatch("updateSession", {
-        session,
-        communityId,
-        userId,
-        accountId,
-        connected,
-      });
-    } catch (err) {
-      return context.dispatch("updateSession", {
-        session: null,
-        communityId: null,
-        userId: null,
-        accountId: null,
-        connected: null,
-      });
+  async init() {
+    // Check if has token in query params and ignore Layers SDK connection
+    const token = getQueryVariable("token");
+    if (token) {
+      console.warn("Using user's token instead of session");
+      return;
     }
-  },
 
-  async updateSession(
-    context,
-    { session, communityId, userId, accountId, bridgeConnected }
-  ) {
-    context.commit("setSession", session);
-    context.commit("setCommunityId", communityId);
-    context.commit("setUserId", userId);
-    context.commit("setAccountId", accountId);
-    context.commit("setBridgeConnected", bridgeConnected);
+    await LayersPortal.readyPromise;
+    if (!LayersPortal.platform) {
+      throw new Error("Layers Portal is not connected");
+    }
+
+    await LayersPortal.connectedPromise;
   },
 };
 
@@ -102,5 +40,4 @@ export default {
   state,
   getters,
   actions,
-  mutations,
 };
