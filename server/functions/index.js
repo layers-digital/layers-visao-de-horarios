ea382cea732a482636f6a2768ce435e51e3682d0
André Sakuma <andre.sakuma@tenda.digital>
2020-12-22 14:15:19 -0300

    add Sentry and /errors/index

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,123 +2,146 @@ const functions = require('firebase-functions');
 const express = require('express');
 const axios = require('axios');
 const _ = require('lodash');
+const Sentry = require('@sentry/node');
+const { BadRequest, AxiosError, InternalError } = require('./errors');
 
 const INTENT = '@layers:education:Timetables:getRelated'
 
-const relatedAPI = async function (req, res) {
-  // Disable CORS when is running locally
-  if(process.env.FUNCTIONS_EMULATOR) {
-    res.set({ 'Access-Control-Allow-Origin': '*' })
-  }
+const app = express()
 
-  const { userToken, community, session, userId } = req.query
+Sentry.init({
+  dsn: functions.config().sentry.dsn,
+  environment: functions.config().sentry.env,
+  attachStacktrace: true,
+})
+app.use(Sentry.Handlers.requestHandler())
 
-  if(!userToken && !session) {
-    return res.status(400).send({error: `user token or session not provided`})
-  }
+app.use(cors())
 
-  if(!userToken && session && !userId) {
-    return res.status(400).send({error: `userId not provided`})
-  }
-
-  if(!community) {
-    return res.status(400).send({error: `community of user not provided`})
-  }
-
-  const Layers = axios.create({
-    baseURL: functions.config().layers.api,
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'community-id': community
-    }
-  })
-
-  // Fetch user data
-  let userData = null
-  if(session) {
-    try {
-      await Layers.get('/sso/session/validate', {
-        params: { community, session, userId },
-        headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
-      })
-    } catch(err) {
-      return res.status(401).send({error: `Invalid session`})
-    }
-
-    try {
-      let res = await Layers.get(`/users/${userId}`, {
-        headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
-      })
-      userData = res.data
-    } catch(err) {
-      return res.status(401).send({error: `Error fetching user data`})
-    }
-  } else {
-    // Deprecated method to validate user token
-    try {
-      let res = await Layers.get('/user/me/', {
-        headers: { 'Authorization': `Bearer ${userToken}`}
-      })
-      userData = res.data
-    } catch(err) {
-      return res.status(401).send({error: `Error fetching user data using deprecated method`})
-    }
-  }
-
-  // Discovery providers
-  let providers = []
+app.get('/', async function (req, res) {
   try {
-    let res = await Layers.get(`/services/discover/${INTENT}?version=1`,
-    {
-      headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
-    })
-    providers = res.data
-  } catch(err) {
-    return res.status(500).send({error: `Erro ao buscar dados dos providers no Layers` })
-  }
-
-  // Call intents
-  const promises = providers.map(async provider => {
-    const data = {
-      user: {
-        id: userData._id,
-        name: userData.name,
-        alias: userData.alias,
-        // timezone: String,  // Fuso horário do usuário
-        // language: String,  // Língua preferencial do usuário
-        // accountId: String,  // ID da account do usuário
-      },
+    // Disable CORS when is running locally
+    if(process.env.FUNCTIONS_EMULATOR) {
+      res.set({ 'Access-Control-Allow-Origin': '*' })
     }
-    try {
-      return {
-        status: 'success',
-        provider: provider,
-        payload: await Layers.post(`/services/call/${INTENT}/${provider.id}?version=1&timeout=10000`, data,
-        {
+
+    const { userToken, community, session, userId } = req.query
+
+    if(!userToken && !session) {
+      throw new BadRequest('user token or session not provided')
+    }
+
+    if(!userToken && session && !userId) {
+      throw new BadRequest('userId not provided')
+    }
+
+    if(!community) {
+      throw new BadRequest('community of user not provided')
+    }
+
+    const Layers = axios.create({
+      baseURL: functions.config().layers.api,
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'community-id': community
+      }
+    })
+
+    // Fetch user data
+    let userData = null
+    if(session) {
+      try {
+        await Layers.get('/sso/session/validate', {
+          params: { community, session, userId },
           headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
         })
+      } catch(err) {
+        if(!err.response) throw new InternalError(err.message)
+        throw new AxiosError(err)
       }
+
+      try {
+        let res = await Layers.get(`/users/${userId}`, {
+          headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
+        })
+        userData = res.data
+      } catch(err) {
+        if(!err.response) throw new InternalError(err.message)
+        throw new AxiosError(err)
+      }
+    } else {
+      // Deprecated method to validate user token
+      try {
+        let res = await Layers.get('/user/me/', {
+          headers: { 'Authorization': `Bearer ${userToken}`}
+        })
+        userData = res.data
+      } catch(err) {
+        if(!err.response) throw new InternalError(err.message)
+        throw new AxiosError(err)
+      }
+    }
+
+    // Discovery providers
+    let providers = []
+    try {
+      let res = await Layers.get(`/services/discover/${INTENT}?version=1`,
+      {
+        headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
+      })
+      providers = res.data
     } catch(err) {
-      return {
-        status: 'error',
-        provider: provider,
-        payload: err
+      if(!err.response) throw new InternalError(err.message)
+      throw new AxiosError(err)
+    }
+
+    // Call intents
+    const promises = providers.map(async provider => {
+      const data = {
+        user: {
+          id: userData._id,
+          name: userData.name,
+          alias: userData.alias,
+          // timezone: String,  // Fuso horário do usuário
+          // language: String,  // Língua preferencial do usuário
+          // accountId: String,  // ID da account do usuário
+        },
       }
-    }
-  })
-  let providersData = await Promise.all(promises)
-  let payload = []
+      try {
+        return {
+          status: 'success',
+          provider: provider,
+          payload: await Layers.post(`/services/call/${INTENT}/${provider.id}?version=1&timeout=10000`, data,
+          {
+            headers: { 'Authorization': 'Bearer ' + functions.config().layers.secret }
+          })
+        }
+      } catch(err) {
+        return {
+          status: 'error',
+          provider: provider,
+          payload: err
+        }
+      }
+    })
+    let providersData = await Promise.all(promises)
+    let payload = []
 
-  providersData.forEach(data => {
-    let providerPayload = {
-      provider: data.provider,
-      result: _.get(data, 'payload.data.data.result', []),
-    }
-    payload.push(providerPayload)
-  })
+    providersData.forEach(data => {
+      let providerPayload = {
+        provider: data.provider,
+        result: _.get(data, 'payload.data.data.result', []),
+      }
+      payload.push(providerPayload)
+    })
 
-  return res.status(200).send(payload)
-}
+    return res.status(200).send(payload)
+  } catch (error) {
+    return next(error)
+  }
+})
+
+app.use(Sentry.Handlers.errorHandler())
 
 // Expose Express API as a single Cloud Function:
 exports.related = functions.https.onRequest(relatedAPI);
