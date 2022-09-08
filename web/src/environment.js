4a0895c8704ea673d4ca86585c4920924fce1da8
Kevyn Tuleu Dourado <kevyn.tuleu@layers.education>
2022-09-08 17:43:45 -0300

    ✨ add new logEvent

--- web/src/environment.js
+++ web/src/environment.js
@@ -1,30 +1,92 @@
 // TODO: Do not check URL in order to know the environment
 const URL = window.location.hostname;
 function isLocal() {
-  if (URL == "localhost") return true;
-  if (URL.includes("192.168")) return true;
+  if (URL == 'localhost') return true;
+  if (URL.includes('192.168')) return true;
   return false;
 }
 
 function isStaging() {
-  if (URL.includes("layers-horarios-staging")) return true;
+  if (URL.includes('layers-horarios-staging')) return true;
   return false;
+}
+
+function LayersApiLocation() {
+  // Handle local development
+  if (isLocal()) {
+    if (URL == 'localhost') return 'http://localhost:8009/v1';
+    if (URL.includes('192.168')) return URL.replace(/:\d+.*/, ':8009/v1');
+  }
+
+  if (isStaging()) {
+    return 'https://api.staging.layers.digital/v1';
+  }
+
+  // Defaults to production api
+  return 'https://api.layers.digital/v1';
 }
 
 function ApiLocation() {
   // Handle local development
   if (isLocal()) {
-    return "http://localhost:8060/layers-visao-de-horarios/us-central1/api/api/";
+    return 'http://localhost:8060/layers-visao-de-horarios/us-central1/api/api/';
   }
 
   if (isStaging()) {
-    return "/api";
+    return '/api';
   }
 
   // Defaults to production api
-  return "/api";
+  return '/api';
+}
+
+function getGAEnvs() {
+  if (isLocal() || isStaging()) {
+    // CRIAR PROJETO NO FIREBASE E PEGAR ENVS
+    return {
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: '',
+      appId: '',
+      measurementId: '',
+    };
+  }
+  // PEGAR ENVS
+  return {
+    apiKey: 'AIzaSyClHapIUfABfG7HlGLX7MldyTJMHqlO36w',
+    authDomain: 'layers-visao-de-horarios.firebaseapp.com',
+    databaseURL: 'https://layers-visao-de-horarios.firebaseio.com',
+    projectId: 'layers-visao-de-horarios',
+    storageBucket: 'layers-visao-de-horarios.appspot.com',
+    messagingSenderId: '357333412353',
+    appId: '1:357333412353:web:1e37658fa7c596e5a36e00',
+    measurementId: 'G-230LP96Q3H',
+  };
+}
+
+function getAmplitudeEnvs() {
+  //DEFINIR ORGANIZACAO DO AMPLITUDE E PEGAR ENVS
+  if (isLocal() || isStaging()) {
+    return {
+      apiKey: '',
+    };
+  }
+
+  return {
+    apiKey: '',
+  };
 }
 
 export default {
   API_URL: ApiLocation(),
+  LAYERS_API_URL: LayersApiLocation(),
+
+  // Google Analytics
+  GA_CONFIG: getGAEnvs(),
+
+  // Amplitude Analytics
+  AMPLITUDE_CONFIG: getAmplitudeEnvs(),
 };
--- web/src/main.js
+++ web/src/main.js
@@ -1,36 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Axios from "axios";
-import createStore from "./store";
-import getQueryVariable from "@/helpers/getQueryVariable";
-import General from "./styles/General.css";
-import Grid from "./styles/Grid.css";
-import Colors from "./styles/Colors.css";
-import Transitions from "./styles/Transitions.css";
-import Text from "./styles/Text.css";
-import Utils from "./styles/Utils.css";
-import Environment from "./environment";
-import * as firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/performance";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyClHapIUfABfG7HlGLX7MldyTJMHqlO36w",
-  authDomain: "layers-visao-de-horarios.firebaseapp.com",
-  databaseURL: "https://layers-visao-de-horarios.firebaseio.com",
-  projectId: "layers-visao-de-horarios",
-  storageBucket: "layers-visao-de-horarios.appspot.com",
-  messagingSenderId: "357333412353",
-  appId: "1:357333412353:web:1e37658fa7c596e5a36e00",
-  measurementId: "G-230LP96Q3H",
-};
-
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-firebase.performance();
-
-Vue.prototype.$analytics = firebase.analytics();
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import Axios from 'axios';
+import createStore from './store';
+import getQueryVariable from '@/helpers/getQueryVariable';
+import General from './styles/General.css';
+import Grid from './styles/Grid.css';
+import Colors from './styles/Colors.css';
+import Transitions from './styles/Transitions.css';
+import Text from './styles/Text.css';
+import Utils from './styles/Utils.css';
+import Environment from './environment';
 
 Vue.config.productionTip = false;
 
@@ -42,12 +22,12 @@ async function init() {
     await LayersPortal.connectedPromise;
     userId = LayersPortal.userId;
   } catch (err) {
-    userId = getQueryVariable("token");
+    userId = getQueryVariable('token');
   }
 
   const store = createStore({ userId });
 
-  await store.dispatch("layers/init");
+  await store.dispatch('layers/init');
 
   new Vue({
     router,
@@ -62,7 +42,7 @@ async function init() {
     render: function (h) {
       return h(App);
     },
-  }).$mount("#app");
+  }).$mount('#app');
 }
 
 init();
